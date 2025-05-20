157522a9833815aac10ccd8ea4b66c98bbcad43c
Behroozi0149 <tahabehroozi298@gmail.com>
2025-05-20 08:17:31 +0330

    update bot

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from gtts import gTTS
 import os
+from colorama import Fore, Style, init
 
 TOKEN = '7307621135:AAEtDz5g5jxexa4LVfMVskjulQVoSWe3AFA'
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! متن خودتو بفرست تا تبدیل به صداش کنم.")
+
 
 async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
     if not text.strip():
         await update.message.reply_text("لطفا یک متن معتبر بفرست.")
+        await update.message.reply_text(":(")
         return
 
     tts = gTTS(text=text, lang='en')
@@ -23,11 +27,12 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     os.remove(filename)
 
+
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler('start', start))
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_text))
 
-    print("Bot is running...")
+    print(Fore.LIGHTGREEN_EX + "Bot is running...   :)" + Style.RESET_ALL)
     app.run_polling()
